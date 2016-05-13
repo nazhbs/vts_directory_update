e174c74a5ffa613984dfab4b3d6bff627bcc4476
Nazmus Nasir <nnasir@hbs.edu>
2016-05-13 11:15:54 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,14 @@
 <?php
+/*
+VTS Directory Update Script
+Author: Nazmus Nasir
+URL: http://nazm.us 
+Harvard Business School Information Technology - Learning Management Ecosystem
+
+This script's purpose is to generate the HTML for an internal directory - has no real use outside of this one usecase. 
+
+Config.php - this file contains the local MySQL settings
+*/
 
 $server = "localhost"; //server name/url
 $username = "root"; //MySQL Username
@@ -11,12 +21,4 @@ $conn = mysqli_connect($server, $username, $password, $db);
 if(mysqli_connect_errno()){
 		echo "DB Connection failed: ". mysqli_connect_error(); //error checking
 	} 
-
-
-//other conneciton type
-//	$select_db = mysqli_select_db($conn,$db);
-//	if(!$select_db){
-//		echo "DB Selection Failed: ". mysqli_error();
-//	}
-//else echo "connection successful";
 ?>
